c47dbb59f68841a68d820b0d8fd2ca4dad8c110e
soma-tepes <114878779+soma-tepes@users.noreply.github.com>
2023-08-22 14:48:03 -0700

    a

--- src/assets/components/AddComponents/CaptureHores.jsx
+++ src/assets/components/AddComponents/CaptureHores.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react'
 import SubDataPicker from './SubDataPicker'
 import Intruccion from './Intruccion'
-
+import '../../Styles/Datapicker.css'
 const CaptureHores = () => {
 
 
     return (
-        <div className='captureHrs' >
+        <>
+            <h2 className='captureh2'>Calculadora de Horas Actuales - SAP Data (0.3v)
+            </h2>
+            <div className='captureHrs' >
 
-            <div>
-                <SubDataPicker />
+                <div>
+                    <SubDataPicker />
+                </div>
+                <div>
+                    <Intruccion />
+                </div>
             </div>
-             <div>
-                <Intruccion />
-            </div>
-        </div>
+        </>
+
     )
 }
 
--- src/assets/components/AddComponents/SubDataPicker.jsx
+++ src/assets/components/AddComponents/SubDataPicker.jsx
@@ -1,42 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import TimePicker from 'react-time-picker';
+import moment from 'moment';
 import 'react-time-picker/dist/TimePicker.css';
 import 'react-clock/dist/Clock.css';
-import '../../Styles/CaptureHours.css'
-
+import '../../Styles/CaptureHours.css';
 
 const SubDataPicker = () => {
-    const [time, setTime] = useState('00:00');
-    const [time2, setTime2] = useState('00:00');
+  const [time, setTime] = useState('00:00');
+  const [time2, setTime2] = useState('00:00');
+  const [input, setinput] = useState()
 
-    const rangeTime = 87.50
+  let rangeTime = (21 / 24) * 100;
+
+
+
+  const getTimeDifference = () => {
+    const startTime = moment(time, 'HH:mm');
+    const endTime = moment(time2, 'HH:mm');
+    const duration = moment.duration(endTime.diff(startTime));
+    const hours = duration.asHours();
+    return hours * rangeTime / 100
+    ;
+  };
+
+  const dataFinal = getTimeDifference() * input
   return (
-
     <div className='datapicker'>
-            <div className='range'>
-            <h3>CaptureHores-- </h3>
-            <p>{`${rangeTime}%`}</p>
-       
-            </div>
-          
-            <div >
-                <TimePicker
-                    format="HH:mm"
-                    value={time}
-                    onChange={setTime}
-                />
-                   <TimePicker
-                    format="HH:mm"
-                    value={time2}
-                    onChange={setTime2}
-                />
-            </div>
-            <div>
-              <span>Capture Employe!</span>
-                <input type="number" />
-            </div>
-            </div>
+      <div className='range'>
+        <h3>-- CaptureHores -- </h3>
+        <p>{`Product Time: ${rangeTime}%`}</p>
+        <p>{`Time Range: ${getTimeDifference().toFixed(2)} hours`}</p>
+      </div>
+
+      <div className='datetime'>
+        <TimePicker className="ran"
+          format="HH:mm"
+          value={time}
+          onChange={setTime}
+        />
+        
+        <TimePicker
+          format="HH:mm"
+          value={time2}
+          onChange={setTime2}
+        />
+      </div>
+
+      <div>
+        <span>Capture Employe!</span>
+        <input type="number"  value={input} onChange={e=>setinput(e.target.value)}/>
+      </div>
+      {}
+      Time data capturate {input && dataFinal.toFixed(2)}
+    </div>
   )
 }
 
-export default SubDataPicker
+export default SubDataPicker;
