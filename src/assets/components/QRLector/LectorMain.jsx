4470bb5236d192a59b8ce074a72425576a7d0bc9
soma-tepes <114878779+soma-tepes@users.noreply.github.com>
2023-09-20 14:47:11 -0700

    a

--- src/assets/components/QRLector/LectorMain.jsx
+++ src/assets/components/QRLector/LectorMain.jsx
@@ -3,23 +3,44 @@ import '../../Styles/Lector.css'
 const LectorMain = () => {
 
     const [inputScan, setInputScan] = useState([])
-
+  
     const handleSubmit = (e) => {
         e.preventDefault()
-        const data = e.target.datas.value
-        inputScan.length == 0 ? setInputScan([data]) : 
-        setInputScan([data,...inputScan])
-        e.target.datas.value =""
-        console.log(inputScan)
-    }
+        const data = e.target.datas.value.split(" ").filter(e=>e != "" && e )
+        
+        if(data.length === 18) { 
 
+            const dataGral = {
+                name : data[0],
+                description : data[1],
+                lote : data[11],
+                qty : data[13],
+                wo : data[16],
+            }
+            setInputScan(dataGral)
+        }
+        if(data.length === 5) {
+            const dataGral = {
+                name : data[0],
+              /*   description : data[1], */
+                lote : data[4],
+                qty : data[2],
+                wo : data[3].slice(0,10),
+            }
+            setInputScan(dataGral)
+        } 
+
+       else{ console.log("Not Data")}
+        e.target.datas.value = ""
+    }
+console.log(inputScan)
 
     return (
         <div className='containerLector'>
             <div className='lectorMain'>
                 <div>  Scan Ticket
                     <form onSubmit={handleSubmit}>
-                        <input type="text" required  name='datas'/>
+                        <input type="text" required  name='datas'  />
                         <button>Send</button>
                     </form>
 
@@ -29,17 +50,19 @@ const LectorMain = () => {
                         <tr>
                             <th>Model</th>
                             <th>Description</th>
-                            <th>QTY</th>
-                            <th>PO</th>
                             <th>Lote</th>
-                            <th>Date</th>
+                            <th>QTY</th>
+                            <th>Wo</th>
+                            <th>PO</th>
 
                         </tr>
                         <tr>
-                            <td>Subtitle</td>
-                            <td>Subtitle2</td>
-                            <td>Subtitle2</td>
-                            <td>Subtitle2</td>
+                            <td>{inputScan.name  }</td>
+                            <td>{inputScan.description  }</td>
+                            <td>{inputScan.lote  }</td>
+                            <td>{inputScan.qty  }</td>
+                            <td>{inputScan.wo  }</td>
+                           
 
                         </tr>
                     </table>
